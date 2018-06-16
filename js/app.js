3fd6e9b21c266448a3d9c1c5527e72cc9b0ff953
Steve <sbrennison@gmail.com>
2018-06-16 14:34:38 -0500

    Concepts through Let's Get Even completed

---
+++ js/app.js
@@ -0,0 +1,77 @@
+
+// Conceptual questions
+// 1. With const or let
+// 2. By listing the variable and reassigning the value if let. If const, we have to use a function.
+// 3. Concatenation or interpolation
+
+
+// Strings
+// 1.
+let firstVariable = "Hello World";
+  firstVariable = 5;
+  secondVariable = firstVariable;
+  secondVariable = "word";
+  console.log(firstVariable);
+
+// 2.
+  yourName = "Steve";
+  greeting = "Hello, my name is ";
+  console.log(greeting + yourName);
+
+// // Booleans
+  // const a = 4;
+  // const b = 53;
+  // const c = 57;
+  // const d = 16;
+  // const e = 'Kevin';
+
+  // a < b;
+  // c > d;
+  // 'Name' === 'Name';
+  // a < b !== c;
+  // a = a < d;
+  // e === 'Kevin';
+  // 48 !== '48';
+
+// // Farm
+//   let animal = true;
+//     if(animal === "cow") {
+//       console.log("Mooooo!");
+//     } else {
+//       console.log("Hey! You're not a cow!");
+//     }
+
+  
+// // Driver's Ed
+// let age = i;
+//   if(i >= 16) {
+//     console.log("Here are the keys");
+//   } else {
+//     console.log("Sorry, you're too young");
+//   }
+
+//   console.log(15);
+
+
+// // Just Loop It
+// for(let i = 0; i <= 10; i++){
+//   console.log(i);
+// }
+
+// for(let i = 10; i <= 4000; i++){
+//   console.log(i);
+// }
+
+// for(let i = 10; i <= 4000; i += 2){
+//   console.log(i);
+// }
+
+
+// Let's Get Even
+
+
+
+
+
+
+
